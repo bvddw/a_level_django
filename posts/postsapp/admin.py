61a88883a6a4109cd37890ec6baf250e5ebc3524
bvddw <galaid.arseniy@gmail.com>
2023-08-03 04:21:00 +0300

    added table usertopic (m2m rel) to admin registration

--- posts/postsapp/admin.py
+++ posts/postsapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Topic, Article, Comment
+from .models import Topic, Article, Comment, UserTopic
 
 admin.site.register(Topic)
 admin.site.register(Article)
 admin.site.register(Comment)
+admin.site.register(UserTopic)
